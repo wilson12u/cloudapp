eb0a5761a1952cfffac4744dd14f1a023b63bdf5
wilson <wilsonnelson86@gmail.com>
2022-09-20 07:41:43 -0700

    initial commit

---
+++ src/main/java/com/wilson/cloudapp/controller/BaseController.java
@@ -0,0 +1,16 @@
+package com.wilson.cloudapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class BaseController {
+    
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+
+    }
+    
+}
